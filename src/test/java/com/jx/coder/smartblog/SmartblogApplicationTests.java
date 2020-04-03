b6a4cf461c72584de03bf259d4c20764be89f02f
qingxuan <1834097423@qq.com>
2020-04-03 22:30:02 +0800

    初始项目

---
+++ src/test/java/com/jx/coder/smartblog/SmartblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jx.coder.smartblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
